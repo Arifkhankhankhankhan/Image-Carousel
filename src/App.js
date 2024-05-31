8d86c2ce0beab81a3c2650d07576f03b3b1fa492
Arifkhankhankhankhan-Git <arifkhan93246057@gmail.com>
2024-05-31 08:02:14 -0700

    react commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const images = [
+  "https://picsum.photos/500/400/?random/1",
+  "https://picsum.photos/500/400/?random/10",
+  "https://picsum.photos/500/400/?random/20",
+  "https://picsum.photos/500/400/?random/30",
+  "https://picsum.photos/500/400/?random/40"
+];
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const width = 500;
+
+  const prevImage = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const nextImage = () => {
+    if (count < images.length - 1) {
+      setCount(count + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="image-container" style={{ transform: `translateX(-${width * count}px)` }}>
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`random ${index}`} />
+        ))}
+      </div>
+      <div className="button-container">
+        <button onClick={prevImage}>Prev</button>
+        <button onClick={nextImage}>Next</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
